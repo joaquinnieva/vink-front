0763edb993c2ff494e914cccb6f30ac8ebfe339c
joaquinnieva <joacoo.nieva@gmail.com>
2022-02-19 23:07:03 -0300

    router fixed

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -7,17 +7,21 @@ import Register from '../pages/Register/Register';
 
 function Router() {
   const user = useSelector((state) => state.auth.session);
-
+  const userRoute = user?.username;
   return (
     <Routes>
-      {!user && (
+      {user ? (
+        <>
+          <Route exact path="/login" element={<Navigate to={'/' + userRoute} />} />
+          <Route exact path="/register" element={<Navigate to={'/' + userRoute} />} />
+        </>
+      ) : (
         <>
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
         </>
       )}
       <Route exact path="/:username" element={<Profile />} />
-      <Route exact path="/profile" element={<Profile />} />
       <Route exact path="/home" element={<Navigate to="/" />} />
       <Route exact path="/" element={<Home />} />
       {/* <Route exact path="/error" element={<NotFound404 />} /> */}
