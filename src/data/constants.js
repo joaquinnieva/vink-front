d73a0429376d84313f359c74cc5d578159b59cc3
joaquinnieva <joacoo.nieva@gmail.com>
2022-02-21 04:19:36 -0300

    constants updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -45,4 +45,6 @@ export const COPY_SUCCESS = 'Copiado con éxito';
 export const MY_LINK = 'Mi enlace';
 export const BG_COLOR = 'Color de fondo';
 export const TEXT_COLOR = 'Color de texto';
+export const LEARN_MORE = 'Saber más';
 export const ERROR_MSG = 'Algo ha salido mal, intentalo más tarde';
+export const DEVELOPED_BY = 'Desarrollado por';
